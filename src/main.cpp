7f953712c755512eb70b17d351f5742e8314c82b
Jarek Jeda <jarek.jeda@gmail.com>
2026-08-07 15:48:55 +0200

    vcpkg_config

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,16 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include <glm/glm.hpp>
-#include <imgui/imgui.h>
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
 #include <sol/sol.hpp>
 
-int main() 
-	std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
 
+	lua.script("print('Sol2 & Lua works!')");
+	std::cout << "Engine works!" << std::endl;
 	return 0;
 }

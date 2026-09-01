a818a62c197e976ab6386c8a4468cfe226689210
Jarek Jeda <jarek.jeda@gmail.com>
2026-09-01 16:04:12 +0200

    class_Pool()

--- src/ecs.cpp
+++ src/ecs.cpp
@@ -4,3 +4,23 @@ int Entity::getId() const {
 	return id;
 }
 
+void System::addEntityToSystem(Entity entity) {
+	entities.push_back(entity);
+};
+
+void System::removeEntityFromSystem(Entity entity) {
+	entities.erase(std::remove_if(entities.begin(), entities.end(),
+		[&entity](Entity e) { 
+			return entity == e; 
+		}), entities.end());
+};
+
+std::vector<Entity> System::getSystemEntities() const {
+	return entities;
+};
+
+const Signature& System::getComponentSignature() const {
+	return componentSignature;
+};
+
+
--- src/ecs.hpp
+++ src/ecs.hpp
@@ -31,6 +31,11 @@ class Entity {
 	public:
 		Entity(int id) : id(id) {};
 		int getId() const;
+
+		// Overload the equality operator to compare entities based on their IDs.
+		bool operator ==(const Entity& other) const {
+			return id == other.id;
+		}
 };
 
 // SYSTEM
@@ -47,14 +52,86 @@ class System {
 		void addEntityToSystem(Entity entity);
 		void removeEntityFromSystem(Entity entity);
 		std::vector<Entity> getSystemEntities() const;
-		Signature& getComponentSignature() const;
+		const Signature& getComponentSignature() const;
 
 		// Defines the components that an entity must have to be added to this system.
 		template<typename tComponent> void requireComponent();
 };
 
-class Registry {
+class iPool {
+	public:
+		virtual ~iPool() {}
+};
 
+// POOL
+// A pool is just a vector (contiguous data) of objects of type T.
+template <typename T>
+class Pool: iPool {
+	private:
+		std::vector<T> data; // Vector of pointers to component data
+
+	public:
+		Pool(int size = 100) {
+			data.resize(size);
+		}
+		virtual ~Pool() = default;
+
+		bool isEmpty() const {
+			return data.empty();
+		}
+
+		int getSize() const {
+			return data.size();
+		}
+
+		void resize(int n) {
+			data.resize(n);
+		}
+
+		void clear() {
+			data.clear();
+		}
+
+		void add(T object) {
+			data.push_back(object);
+		}
+
+		void set(int index, T object) {
+			data[index] = object;
+		}
+
+		T& get(int index) {
+			return static_cast<T&>(data[index]);
+		}
+
+		T& operator [](unsigned int index) {
+			return data[index];
+		
+		}
+};
+
+// REGISTRY
+// The Registry manages the creation and destruction of entities, 
+// as well as the addition and removal of components from entities.
+class Registry {
+	private:
+		// Keep track of number of entities added to the scene.
+		int numEntities = 0;
+
+		// Vector of component pools, where each pool contains all the data for the certain component type.
+		// Vector index == component type ID
+		// Pool index == entity ID
+		std::vector<iPool*> componentPools;
+
+	public:
+		//Registry() = default;
+		//~Registry() = default;
+
+		//Entity createEntity();
+		//void destroyEntity(Entity entity);
+		//void addComponentToEntity(Entity entity, int componentId);
+		//void removeComponentFromEntity(Entity entity, int componentId);
+		//const Signature& getEntitySignature(Entity entity) const;
 };
 
 template<typename tComponent>
